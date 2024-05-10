58c0734ba222657a7c0518867551effa543c01fc
ChetouiArij11 <arijchetoui1@gmail.com>
2024-05-10 11:45:10 +0200

    mise ajour 409

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MedecinService } from '../services/medecin.service';
+import { Medecin } from '../models/medecins.model';
 
 @Component({
   selector: 'searchbar',
@@ -9,14 +10,18 @@ import { MedecinService } from '../services/medecin.service';
 export class SearchbarComponent {
 
   nomMedecin: string = '';
-  telMedecin: string = '';
-  emailMedecin: string = '';
+  specialite: string = '';
+
   searchResults: any[] = [];
+  @Input() medecin: Medecin | undefined;
+
+
+  @Output() medecinSelected: EventEmitter<number> = new EventEmitter<number>();
 
   constructor(private medecinService: MedecinService) {}
 
   searchMedecin() {
-    this.medecinService.searchMedecin(this.nomMedecin, this.telMedecin, this.emailMedecin)
+    this.medecinService.searchMedecin(this.nomMedecin, this.specialite)
       .subscribe(
         (response: any[]) => {
           this.searchResults = response; // Stockez les résultats dans la propriété searchResults
@@ -26,5 +31,14 @@ export class SearchbarComponent {
           alert('Une erreur s\'est produite lors de la recherche de médecins.');
         }
       );
+  }
+
+  prendreRendezVous(id: number | undefined) {
+    if (id !== undefined) {
+      console.log('Selected medecin ID:', id); // Afficher l'identifiant du médecin sélectionné
+      this.medecinSelected.emit(id);
+    } else {
+      console.error('L\'identifiant du médecin est indéfini.');
+    }
   }
 }
--- src/app/services/medecin.service.ts
+++ src/app/services/medecin.service.ts
@@ -17,27 +17,23 @@ export class MedecinService {
   }
   constructor(private http: HttpClient) { }
 
-  searchMedecin(nom: string, telMedecin: string, email: string): Observable<any> {
+  searchMedecin(nom: string, specialite: string): Observable<any> {
     // Paramètres de la requête
     let params = new HttpParams();
     if (nom) {
       params = params.append('nom', nom);
     }
-    if (telMedecin) {
-      params = params.append('telMedecin', telMedecin.toString());
+    if (specialite) {
+      params = params.append('specialite', specialite.toString());
     }
-    if (email) {
-      params = params.append('email', email);
-    }
+
 
     // Construire l'URL en fonction des paramètres fournis
     let url = this.baseUrl + '/search';
     if (nom) {
       url += '/nom';
-    } else if (telMedecin) {
-      url += '/telMedecin';
-    } else if (email) {
-      url += '/email';
+    } else if (specialite) {
+      url += '/specialite';
     }
 
     // Envoi de la requête GET avec les paramètres
